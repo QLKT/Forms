99390359ba836771b7f06ce9de4c356897926aed
QLKT <pqlktmanagement@gmail.com>
2025-12-05 12:08:05 +0700

    Add files via upload

---
+++ QLKT_DulieuHSLuu.js
@@ -0,0 +1,34 @@
+   
+   document.addEventListener('DOMContentLoaded', function(event)
+   {
+      var DropdownMenu1_dropdownToggle = document.querySelectorAll('#DropdownMenu1 .dropdown-toggle');
+      DropdownMenu1_dropdownToggle.forEach(item => 
+      {
+         var dropdown = new bootstrap.Dropdown(item, {popperConfig:{placement:item.getAttribute('data-bs-placement')}});
+      });
+      var DropdownMenu1_dropdown = document.querySelectorAll('#DropdownMenu1 .dropdown');
+      DropdownMenu1_dropdown.forEach(item => 
+      {
+         item.addEventListener('shown.bs.dropdown', function(e)
+         {
+            e.currentTarget.classList.add('show');
+         });
+         item.addEventListener('hidden.bs.dropdown', function(e)
+         {
+            e.currentTarget.classList.remove('show');
+         });
+      });
+   });
+   
+   $(document).ready(function()
+   {
+      $("a[href*='#Heading1']").click(function(event)
+      {
+         event.preventDefault();
+         $('html, body').stop().animate({ scrollTop: $('#wb_Heading1').offset().top }, 600, 'linear');
+      });
+      $("#LayoutGrid5").submit(function(event)
+      {
+         return true;
+      });
+   });
